a79f450cda3fc42591ce38854d2ea0278b1b4022
millekel <millekel@SMG50143.mshome.net>
2024-02-14 11:42:06 -0600

    Quote 1

---
+++ GitPractice/src/gitpractice/GitPractice.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitpractice;
+
+/**
+ *
+ * @author millekel
+ */
+public class GitPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("\"A good programmer is someone who always looks both "
+                + "ways before crossing a one-way street.\" - Doug Linder, computer scientist");
+    }
+
+}
